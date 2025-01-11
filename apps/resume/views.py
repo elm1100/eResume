4574866c8c5f9410d5a66328b2917ab03c877d43
elsa <eellham1100@gmail.com>
2025-01-12 00:59:13 +0300

    test5 commit

--- apps/resume/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render,redirect
-from django.http import HttpResponse 
-
-# Create your views here.
-
-# Landing Page
-def index(request):
-    return render(request,'index.html')
-
